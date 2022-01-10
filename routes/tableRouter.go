56f571d91f89c86667f7a16b4e5dc205d8d0cca1
Shuaib O <shuaib.olurode@decagon.dev>
2022-01-10 12:04:30 +0100

    create table route

---
+++ routes/tableRouter.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"restaurant-management/controllers"
+)
+
+func TableRoutes (incomingRoutes *gin.Engine){
+	incomingRoutes.GET("/tables", controllers.GetTables())
+	incomingRoutes.GET("/tables/:table_id", controllers.GetTable())
+	incomingRoutes.POST("/tables", controllers.CreateTable())
+	incomingRoutes.PATCH("/tables/:table_id", controllers.UpdateTable())
+
+}
