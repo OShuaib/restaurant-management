63589227dafed90e767fd86c2862520f1cf0ec03
Shuaib O <shuaib.olurode@decagon.dev>
2022-01-10 12:05:39 +0100

    create invoice route

---
+++ routes/invoiceRoute.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"restaurant-management/controllers"
+)
+
+func InvoiceRoutes (incomingRoutes *gin.Engine){
+	incomingRoutes.GET("/invoices", controllers.GetInvoices())
+	incomingRoutes.GET("/invoices/:invoice_id", controllers.GetInvoice())
+	incomingRoutes.POST("/invoices", controllers.CreateInvoice())
+	incomingRoutes.PATCH("/invoices/:invoice_id", controllers.UpdateInvoice())
+
+}
