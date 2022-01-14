606ecff87b08e8906dd1e1050f9102aa41d4ac2b
Shuaib O <shuaib.olurode@decagon.dev>
2022-01-14 12:00:39 +0100

    fix pointer issue

--- models/user.go
+++ models/user.go
@@ -8,10 +8,10 @@ import (
 
 type User struct {
 	ID				primitive.ObjectID		`bson:"_id"`
-	First_name		string					`json:"first_name" validate:"required,min=2,max=100"`
-	Last_name		string					`json:"last_name" validate:"required,min=2,max=100"`
+	First_name		*string					`json:"first_name" validate:"required,min=2,max=100"`
+	Last_name		*string					`json:"last_name" validate:"required,min=2,max=100"`
 	Password		string					`json:"Password" validate:"required,min=6"`
-	Email			string					`json:"email" validate:"email,required"`
+	Email			*string					`json:"email" validate:"email,required"`
 	Avatar			string					`json:"avatar"`
 	Phone			string					`json:"phone" validate:"required"`
 	Token			string					`json:"token"`
