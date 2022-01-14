35f2de30281128588d85bcad7dcbbd06db8036ff
Shuaib O <shuaib.olurode@decagon.dev>
2022-01-14 10:50:16 +0100

    fix issue

--- models/food.go
+++ models/food.go
@@ -14,5 +14,5 @@ type Food struct {
 	Created_at	time.Time			`json:"created_at"`
 	Update_at	time.Time			`json:"updated_at"`
 	Food_id		string				`json:"food_id"`
-	Menu_id		string				`json:"menu_id" validate:"required"`
+	Menu_id		*string				`json:"menu_id" validate:"required"`
 }
